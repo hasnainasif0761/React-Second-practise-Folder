780c6d53cb37012e51e1a7502a70daf3cef590d7
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-22 14:45:19 +0500

    Third-Updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
 
 const App = () => (
-    <>
-    <h1>hello</h1>
-    </>
+    <div style={{display:'flex', flexWrap:'wrap'}}>
+    <div style={{width: '300px', textAlign: 'center',margin: '10px', fontFamily: 'Arial, sans-serif',border:'1px solid black', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', padding: '20px', borderRadius: '10px'}}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s" />
+        <h1>Huzaifa Asif</h1>
+        <p>Software Engineer | Web Developer | Tech Enthusiast</p>
+    </div>
+    <div style={{width: '300px', textAlign: 'center',margin: '10px', fontFamily: 'Arial, sans-serif',border:'1px solid black', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', padding: '20px', borderRadius: '10px'}}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s" />
+        <h1>
+            Elara Finch
+        </h1>
+        <p>
+            Celestial Cartographer.
+        </p>
+    </div>
+    <div style={{width: '300px', textAlign: 'center',margin: '10px', fontFamily: 'Arial, sans-serif',border:'1px solid black', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', padding: '20px', borderRadius: '10px'}}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s" />
+        <h1>Ali</h1>
+        <p>
+            Web developer | Electrician
+        </p>
+    </div>
+    <div style={{width: '300px', textAlign: 'center',margin: '10px', fontFamily: 'Arial, sans-serif',border:'1px solid black', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', padding: '20px', borderRadius: '10px'}}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s" />
+        <h1>
+            Sarah Johnson
+        </h1>
+        <p>
+            Digital Marketing Specialist.
+        </p>
+    </div>
+    <div style={{width: '300px', textAlign: 'center',margin: '10px', fontFamily: 'Arial, sans-serif',border:'1px solid black', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', padding: '20px', borderRadius: '10px'}}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s" />
+        <h1>
+            Sarah Johnson
+        </h1>
+        <p>
+            Digital Marketing Specialist.
+        </p>
+    </div>
+    <div style={{width: '300px', textAlign: 'center',margin: '10px', fontFamily: 'Arial, sans-serif',border:'1px solid black', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', padding: '20px', borderRadius: '10px'}}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s" />
+        <h1>
+            Sarah Johnson
+        </h1>
+        <p>
+            Digital Marketing Specialist.
+        </p>
+    </div>
+    <div style={{width: '300px', textAlign: 'center',margin: '10px', fontFamily: 'Arial, sans-serif',border:'1px solid black', boxShadow: '0px 0px 15px rgba(0,0,0,0.1)', padding: '20px', borderRadius: '10px'}}>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnSA1zygA3rubv-VK0DrVcQ02Po79kJhXo_A&s" />
+        <h1>
+            Sarah Johnson
+        </h1>
+        <p>
+            Digital Marketing Specialist.
+        </p>
+    </div>
+    </div>
 )
 
 export default App
